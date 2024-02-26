249ed41cc477ea2d55f1fd0b435c16c30e03951e
Christian Bachl <christian.bachl@hahn-software.io>
2024-02-26 16:04:07 +0100

    Add Unittest

---
+++ TestProject1/WebTest.cs
@@ -0,0 +1,15 @@
+namespace TestProject1
+{
+    public class WebTest
+    {
+        [Fact]
+        public void DemoTest()
+        {
+
+            int i = 1;
+            bool result = false;
+            if (i == 1) result = true;
+            Assert.True(result, "Value should be equal to 1");
+        }
+    }
+}
